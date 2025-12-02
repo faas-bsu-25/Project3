91bc6e3efd15a5f08d22e7aa077e53b4a62a195e
bsucs <122378640+bsucs@users.noreply.github.com>
2025-12-02 10:42:22 -0500

    Updated

--- new-game-project/Scripts/LevelSelect.gd
+++ new-game-project/Scripts/LevelSelect.gd
@@ -1,6 +1,8 @@
 extends Control
-
+@export var Cyprus_button: Button
+@export var Berlin_button: Button
+@export var Warsaw_button: Button
 func _ready():
-	$CyprusButton.pressed.connect(func(): Gamecontroller.load_level("cyprus"))
-	$BerlinButton.disabled = !Gamecontroller.unlocked["berlin"]
-	$WarsawButton.disabled = !Gamecontroller.unlocked["warsaw"]
+	Cyprus_button.pressed.connect(func(): Gamecontroller.load_level("cyprus"))
+	Berlin_button.disabled = !Gamecontroller.unlocked["berlin"]
+	Warsaw_button.disabled = !Gamecontroller.unlocked["warsaw"]
