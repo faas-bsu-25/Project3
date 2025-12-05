67047e718639671eba4284d1b0b9a4f4a203317d
bsucs <122378640+bsucs@users.noreply.github.com>
2025-12-05 13:16:31 -0500

    added scenes and scripts

--- new-game-project/Scripts/Cyprus_LevelController.gd
+++ new-game-project/Scripts/Cyprus_LevelController.gd
@@ -4,14 +4,22 @@ var broadcast_numbers = [6,3,6,4,6]
 var private_key = [5,2,8,3,4]
 var expected_message = ""
 
+@export var PrivateKeyLabel: Label
+@export var BroadcastPlayer: AudioStreamPlayer
+@export var SubmitButton: Button
+@export var InputField: LineEdit
+@export var SuspicionBar: ProgressBar
+
 func _ready():
 	expected_message = decode_message()
 
-	$PrivateKeyLabel.text = "Private Key: %s" % private_key
-	$BroadcastPlayer.stream = preload("res://Sounds/Poacher.mp3")
-	$BroadcastPlayer.play()
+	PrivateKeyLabel.text = "Private Key: %s" % str(private_key)
 
-	$SubmitButton.pressed.connect(_check_input)
+	BroadcastPlayer.stream = preload("res://Sounds/Poacher.mp3")
+	BroadcastPlayer.play()
+
+	SubmitButton.pressed.connect(_check_input)
+
 
 func decode_message():
 	var r = ""
@@ -20,11 +28,12 @@ func decode_message():
 		r += char(v + 65)
 	return r
 
+
 func _check_input():
-	var u = $InputField.text.strip_upper()
+	var u = InputField.text.strip_edges().to_upper()
 
 	if u == expected_message:
 		Gamecontroller.unlock("berlin")
 		Gamecontroller.load_level("berlin")
 	else:
-		$SuspicionBar.add(25)
+		SuspicionBar.value += 25
--- new-game-project/Scripts/MainMenu.gd
+++ new-game-project/Scripts/MainMenu.gd
@@ -1,11 +1,19 @@
 extends Control
+
 @export var play_button: Button
+@export var directions_button: Button
+@export var quit_button: Button
+
 func _ready():
 	play_button.pressed.connect(_start)
-
+	directions_button.pressed.connect(_open_directions)
+	quit_button.pressed.connect(_quit)
 
 func _start():
 	Gamecontroller.load_level("LevelSelect")
+
+func _open_directions():
+	Gamecontroller.load_level("HowToDecode")
 
 func _quit():
 	get_tree().quit()
